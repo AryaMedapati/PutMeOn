9c3e23c1ddc6e23ee00a0c6875f3c2819eddf50d
arya <aryamedapati@example.com>
2024-09-24 13:07:44 -0400

    Added Profile Icon to UI for link

--- src/App.js
+++ src/App.js
@@ -1,44 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-import { app } from './firebase';
-import { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Home from './Home';
-import Stats from './Stats'
-import Messages from './Messages';
-import Playlists from './Playlists';
-import Profile from './Profile';
+import logo from "./logo.svg";
+import "./App.css";
+import { app } from "./firebase";
+import { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { CgProfile } from "react-icons/cg";
+
+import Home from "./Home";
+import Stats from "./Stats";
+import Messages from "./Messages";
+import Playlists from "./Playlists";
+import Profile from "./Profile";
 
 function App() {
   useEffect(() => {
     // Confirm Firebase is initialized
-    console.log('Firebase App:', app);
+    console.log("Firebase App:", app);
   }, []);
   return (
     <Router>
       <nav>
-        <Link to="/" style={{ color: 'blue', textDecoration: 'none', marginRight: '15px' }}>
+        <Link
+          to="/"
+          style={{ color: "blue", textDecoration: "none", marginRight: "15px" }}
+        >
           Home
         </Link>
-        <Link to="/stats" style={{ color: 'blue', textDecoration: 'none', marginRight: '15px' }}>
+        <Link
+          to="/stats"
+          style={{ color: "blue", textDecoration: "none", marginRight: "15px" }}
+        >
           Statistics
         </Link>
-        <Link to="/messages" style={{ color: 'blue', textDecoration: 'none', marginRight: '15px' }}>
+        <Link
+          to="/messages"
+          style={{ color: "blue", textDecoration: "none", marginRight: "15px" }}
+        >
           Messages
         </Link>
-        <Link to="/playlists" style={{ color: 'blue', textDecoration: 'none', marginRight: '15px' }}>
+        <Link
+          to="/playlists"
+          style={{ color: "blue", textDecoration: "none", marginRight: "15px" }}
+        >
           Playlists
         </Link>
-        <Link to="/profile" style={{ color: 'blue', textDecoration: 'none', marginRight: '15px' }}>
-          Profile
+        <Link
+          to="/profile"
+        >
+          <div>
+            <CgProfile className="icon"/>
+          </div>
         </Link>
       </nav>
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/stats' element={<Stats/>} />
-        <Route path='/messages' element={<Messages/>} />
-        <Route path='/playlists' element={<Playlists/>} />
-        <Route path='/profile' element={<Profile/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/stats" element={<Stats />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/playlists" element={<Playlists />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
