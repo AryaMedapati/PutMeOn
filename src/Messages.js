c02c71337b11228f4e3a5ce91025ca683ea3b12f
Jason2.0 <jason2.0@Jasons-MacBook-Pro-3.local>
2024-09-29 23:28:40 -0400

    WIP 1

--- src/Messages.js
+++ src/Messages.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
+import "@blueprintjs/core/lib/css/blueprint.css";
+import { Button, Card } from "@blueprintjs/core";
+
+import "./Profile.css"
+
 const Messages = () => {
   return (
-    <div>
-      <h1>Your Messages</h1>
-      <p>This is the Messages page.</p>
-    </div>
+    <body>
+      <div class="container">
+          <div class="sidebar">
+              <Button 
+                style={{ width: '100%', padding: 0, margin: 0 }}
+                text="Your Profile" />
+              <ul>
+                  <li><a href="#">Your Profile</a></li>
+                  <li><a href="#">Privace and security</a></li>
+                  <li><a href="#">Additional Settings</a></li>
+                  <li><a href="#">TEMP</a></li>
+                  <li><a href="#">TEMP</a></li>
+              </ul>
+          </div>
+          <div class="main-content">
+              <h1>Main Content Area</h1>
+              <p>This is where your main content goes.</p>
+          </div>
+      </div>
+  </body>
   );
 };
 
