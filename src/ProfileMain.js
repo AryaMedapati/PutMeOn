67de75cfe598602b222e8e0c5c631ffdaca594cb
Jason2.0 <jason2.0@Jasons-MacBook-Pro-3.local>
2024-10-05 17:18:34 -0400

    WIP on some other stories

--- src/ProfileMain.js
+++ src/ProfileMain.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { TextArea } from '@blueprintjs/core';
+import { Button, TextArea } from '@blueprintjs/core';
+import { useState } from "react";
 
 const ProfileMain = () => {
+    const [editBio, setEditBio] = useState(false);
+
+
     return (
         <div>
             <h1>Set Profile here</h1>
@@ -15,6 +19,12 @@ const ProfileMain = () => {
                     large={true}
                     placeholder="Put your bio here"
                 />
+            </div>
+            <div>
+                <Button
+                    onClick={() => setEditBio(!editBio)}
+                    text="Your Profile" />
+                    
             </div>
             <p>Change the username and password here</p>
         </div>
